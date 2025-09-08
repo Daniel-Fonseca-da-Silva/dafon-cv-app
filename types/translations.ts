58327b3db9d6d4ebfc026e8a270ae8d631521823
Daniel-Fonseca-da-Silva <daniel117fonseca@gmail.com>
2025-09-08 01:13:38 +0100

    feat: create interface to fix deploy

---
+++ types/translations.ts
@@ -0,0 +1,22 @@
+export interface BenefitItem {
+  icon: string;
+  title: string;
+  description: string;
+}
+
+export interface SocialProofStat {
+  number: string;
+  label: string;
+  color: string;
+}
+
+export interface DemoFeature {
+  title: string;
+  description: string;
+}
+
+export interface DemoStat {
+  number: string;
+  label: string;
+  color: string;
+}
