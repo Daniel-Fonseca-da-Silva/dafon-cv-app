65621a616ab07e2b8fc324ddbd5c2da5c4fec63e
Daniel-Fonseca-da-Silva <daniel117fonseca@gmail.com>
2025-10-27 00:45:34 +0000

    fix: added new translation to spanish

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -13,6 +13,21 @@ export async function generateMetadata(): Promise<Metadata> {
   return {
     title: t('title'),
     description: t('description'),
+    icons: {
+      icon: [
+        { url: '/favicon.ico', sizes: '32x32' },
+        { url: '/logos/logo-16x16.png', sizes: '16x16', type: 'image/png' },
+        { url: '/logos/logo-32x32.png', sizes: '32x32', type: 'image/png' },
+        { url: '/logos/logo-64x64.png', sizes: '64x64', type: 'image/png' },
+        { url: '/logos/logo-128x128.png', sizes: '128x128', type: 'image/png' },
+      ],
+      apple: [
+        { url: '/logos/logo-192x192.png', sizes: '192x192', type: 'image/png' },
+      ],
+      other: [
+        { rel: 'icon', url: '/logos/logo-512x512.png', sizes: '512x512', type: 'image/png' },
+      ],
+    },
   };
 }
 
